e1efcebd4770d61318cdbb9e4328d5a1ec06773f
mrdipenff <mrdipenff@gmail.com>
2026-07-06 12:01:26 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyFakeKey-LG-SHORTENER-2026",
-  authDomain: "lg-shortener.firebaseapp.com",
-  projectId: "lg-shortener",
-  storageBucket: "lg-shortener.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef"
+  apiKey: "AIzaSyD3Yl0BR4o6qEX6MeXYjX6Qjlr5BCid5C8",
+  authDomain: "my-website-242fc.firebaseapp.com",
+  projectId: "my-website-242fc",
+  storageBucket: "my-website-242fc.firebasestorage.app",
+  messagingSenderId: "78108710064",
+  appId: "1:78108710064:web:7b5e79f33721fbc7f71775"
 };
 
+// Next.js runtime compile check loop
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
